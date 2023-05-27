b6340cf89f6484e952035262c6df8d1145bb3c48
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-27 16:12:58 +0300

    Style changes to login nd register pages

--- UrlShortener/UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/UrlShortener/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Net;
-using UrlShortener.Models;
 
 namespace UrlShortener.Controllers
 {
